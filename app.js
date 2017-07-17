462c7d71435dd9524647a56f0487877f5929d4a7
evertonmello <evertonmello@live.com>
2017-07-17 16:15:31 -0300

    das

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 	var express = require('express'),
 	app = express(),
-	mongoose = require('mongoose'),
 	bodyParser = require('body-parser'),
 	debug = require('debug')('appjs'),
 	methodOverride = require('method-override'),
+	mongoose = require('mongoose'),
 	Soldado = require('./model/strompperModel'),
 	passport = require('passport');
     
 
 app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(methodOverride('X-HTTP-Method'));
+
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(methodOverride('X-Method-Override'));
+
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+//asddsa
+app.use(methodOverride('X-HTTP-Method')); //sadsda
 app.use(methodOverride('_method'));
 app.use(methodOverride('Access-Control-Allow-Origin'));
 
