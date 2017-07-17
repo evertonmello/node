2971604e89e8e2d67044cca1126fbf942a310867
evertonmello <evertonmello@live.com>
2017-07-17 15:49:21 -0300

    teste

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 	var express = require('express'),
 	app = express(),
-	mongoose = require('mongoose'),
-	bodyParser = require('body-parser'),
-	debug = require('debug')('appjs'),
 	methodOverride = require('method-override'),
 	Soldado = require('./model/strompperModel'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	debug = require('debug')('appjs'),
+	
 	passport = require('passport');
     
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(methodOverride('X-HTTP-Method'));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(methodOverride('X-Method-Override'));
 app.use(methodOverride('_method'));
+app.use(methodOverride('X-HTTP-Method'));
 app.use(methodOverride('Access-Control-Allow-Origin'));
 
 app.use(passport.initialize());
-
 app.use('/', require('./routes'));
 
 module.exports = app;
